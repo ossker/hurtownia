640c8bedabebd93a49fab5724fc46f3b16c865cf
Jakub Knioła <jakubkniola@outlook.com>
2025-04-16 19:00:05 +0200

    fix: update data

--- data/raw/postgres/schema.sql
+++ data/raw/postgres/schema.sql
@@ -4,15 +4,23 @@ DROP TABLE IF EXISTS wojewodztwo;
 
 CREATE TABLE IF NOT EXISTS wojewodztwo (
     id SERIAL PRIMARY KEY,
-    nazwa VARCHAR(100) NOT NULL
+    nazwa VARCHAR(100) NOT NULL,
+    powierzchnia DECIMAL(10, 2) NOT NULL,
+    liczba_ludnosci INTEGER NOT NULL,
+    tablica_rejestracyjna VARCHAR(10) NOT NULL,
+    kod_teryt VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS miasto (
     id SERIAL PRIMARY KEY,
     nazwa VARCHAR(100) NOT NULL,
     wojewodztwo_id INTEGER NOT NULL,
+    powierzchnia DECIMAL(10, 2) NOT NULL,
+    liczba_ludnosci INTEGER NOT NULL,
+    tablica_rejestracyjna VARCHAR(10) NOT NULL,
     FOREIGN KEY (wojewodztwo_id) REFERENCES wojewodztwo(id)
 );
+
 
 CREATE TABLE IF NOT EXISTS uczelnia (
     id SERIAL PRIMARY KEY,
